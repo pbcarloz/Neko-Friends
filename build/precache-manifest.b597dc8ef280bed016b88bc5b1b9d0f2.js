f572be2422cd4e785fa17c6972002350c1be7bb9
Carlos Barcenas <60458360+pbcarloz@users.noreply.github.com>
2020-05-05 22:05:22 -0500

    Add files via upload

---
+++ build/precache-manifest.b597dc8ef280bed016b88bc5b1b9d0f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867821ce8eaee3a6f2d6385be09b02bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "53405f6956e7b75194ab",
+    "url": "/static/css/main.cdedee81.chunk.css"
+  },
+  {
+    "revision": "573e9c90aacac20c725d",
+    "url": "/static/js/2.8fde3f13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8fde3f13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53405f6956e7b75194ab",
+    "url": "/static/js/main.1bd24f04.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  }
+]);
